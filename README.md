a4bc46e8a22460a2a904a98a39a0c034d3482919
Mikhail Knyazhev <markus621@gmail.com>
2019-11-13 03:15:36 +0300

    Topological sorting. Kahn's Algorithm.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# algorithms
+# Algorithms
 Algorithmic calculation methods
+
+
+### 1) Topological sorting. Kahn's Algorithm.
+
+graph/kahn/type.go:2
+
+```go
+import (
+    graphkahn "https://github.com/deweppro/algorithms/graph/kahn"
+ )
+
+kahn := graphkahn.New()
+
+kahn.Add("1", "2")
+kahn.Add("1", "3")
+kahn.Add("3", "4")
+kahn.Add("2", "4")
+kahn.Add("4", "5")
+
+kahn.Build()
+result := kahn.Result()
+```
+
+**Result:** [1,2,3,4,5]
---
+++ graph/kahn/type_test.go
@@ -0,0 +1,54 @@
+package kahn
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnit_KahnCoherent(t *testing.T) {
+	kahn := New()
+
+	assert.NoError(t, kahn.Add("a", "b"))
+	assert.NoError(t, kahn.Add("a", "c"))
+	assert.NoError(t, kahn.Add("a", "d"))
+	assert.NoError(t, kahn.Add("a", "e"))
+	assert.NoError(t, kahn.Add("b", "d"))
+	assert.NoError(t, kahn.Add("c", "d"))
+	assert.NoError(t, kahn.Add("c", "e"))
+	assert.NoError(t, kahn.Add("d", "e"))
+
+	assert.NoError(t, kahn.Build())
+
+	result := kahn.Result()
+	assert.True(t, len(result) > 0)
+
+	assert.Contains(t, []string{"a,b,c,d,e", "a,c,b,d,e"}, strings.Join(result, ","))
+}
+
+func TestUnit_KahnCyclical(t *testing.T) {
+	kahn := New()
+
+	assert.NoError(t, kahn.Add("1", "2"))
+	assert.NoError(t, kahn.Add("2", "3"))
+	assert.NoError(t, kahn.Add("3", "2"))
+
+	assert.Error(t, kahn.Build())
+}
+
+func Benchmark_Kahn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		func() {
+			kahn := New()
+
+			kahn.Add("1", "2")
+			kahn.Add("1", "3")
+			kahn.Add("3", "4")
+			kahn.Add("2", "4")
+			kahn.Add("4", "5")
+
+			kahn.Build()
+		}()
+	}
+}
