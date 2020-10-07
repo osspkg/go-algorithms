697d86d874f0abde555ff3a280f7a3eaa18e628a
Mikhail Knyazhev <markus621@gmail.com>
2020-10-08 01:36:37 +0300

    update lic

--- graph/kahn/type_test.go
+++ graph/kahn/type_test.go
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019-2020 Mikhail Knyazhev <markus621@gmail.com>.
+ * All rights reserved. Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 package kahn
 
 import (
